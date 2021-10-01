4d36f60cefba208ec03585f46728f71e1ab67327
leodeorce <leodeorce@gmail.com>
2021-10-01 18:28:58 -0300

    Tables + some tidying up

--- back-app/src/main/java/plgym/BackAppHtmlController.java
+++ back-app/src/main/java/plgym/BackAppHtmlController.java
@@ -1,14 +1,16 @@
 package plgym;
 
 import java.security.Principal;
+import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
+import com.google.gson.reflect.TypeToken;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import plgym.domain.Exercise;
+import plgym.domain.ExerciseList;
+import plgym.domain.User;
 
 @Controller
 public class BackAppHtmlController
@@ -28,7 +30,14 @@ public class BackAppHtmlController
 	@RequestMapping("/myworkout")
     public String myWorkout(Model model, Principal principal)
 	{
-		model.addAttribute("exerciseDB", BackAppController.exerciseDB);
+		User user = BackAppController.userDB.getValue(principal.getName());
+		String[] userExercisesId = user.getExercisesId().toArray(new String[0]);
+		ExerciseList userExercises = new ExerciseList(
+				userExercisesId,
+				BackAppController.exerciseDB,
+				new TypeToken<Map<String, Exercise>>(){}
+		);
+		model.addAttribute("exercises", userExercises);
         return "myworkout";
     }
 
@@ -39,8 +48,9 @@ public class BackAppHtmlController
 	}
 
 	@RequestMapping("/discover")
-	public String discover()
+	public String discover(Model model)
 	{
+		model.addAttribute("exerciseDB", BackAppController.exerciseDB);
 		return "discover";
 	}
 
--- back-app/src/main/resources/static/js/tables.js
+++ back-app/src/main/resources/static/js/tables.js
@@ -1,34 +1,5 @@
 $(document).ready(function() {
-    $('#table-discover').DataTable( {
-        "language": {
-            "decimal":        ",",
-            "emptyTable":     "Lista vazia",
-            "info":           "Mostrando de _START_ a _END_ dentre _TOTAL_ exercícios",
-            "infoEmpty":      "Mostrando de 0 a 0 dentre 0 exercícios",
-            "infoFiltered":   "(filtrado de _MAX_ exercícios ao todo)",
-            "infoPostFix":    "",
-            "thousands":      " ",
-            "lengthMenu":     "Mostrar _MENU_ exercícios",
-            "loadingRecords": "Carregando...",
-            "processing":     "Processando...",
-            "search":         "Buscar:",
-            "zeroRecords":    "Nenhum exercício correspondente encontrado",
-            "paginate": {
-                "first":      "Ínicio",
-                "last":       "Fim",
-                "next":       "Próximo",
-                "previous":   "Anterior"
-            },
-            "aria": {
-                "sortAscending":  ": ative para ordenar coluna em ordem crescente",
-                "sortDescending": ": ative para ordenar coluna em ordem decrescente"
-            }
-        }
-    } );
-
-    let table = $('#table-myworkout').DataTable( {
-        "paging": false,
-        "searching": true,
+    let tableDiscover = $('#table-discover').DataTable( {
         "language": {
             "decimal":        ",",
             "emptyTable":     "Lista vazia",
@@ -53,20 +24,87 @@ $(document).ready(function() {
                 "sortDescending": ": ative para ordenar coluna em ordem decrescente"
             }
         },
-        "columns": [
+        columns: [
             {
-                defaultContent: '<input type="button" class="btn btn-primary" value="Add"/>'
+                "data": "id",
+                visible: false,
             },
             { "data": "name" },
             { "data": "category" },
             { "data": "difficulty" },
-            { "data": "linkYT" },
-        ],
+            { "data": "link" },
+            { orderable: false },
+        ]
+    } );
+
+    $('#table-discover tbody').on('click', '.btn', function () {
+        let row = $(this).closest('tr');
+        let id = tableDiscover.row(row).data().id;
+        $.ajax("http://localhost:8080/user/exercises", {
+            type : 'PUT',
+            contentType : 'application/json',
+            data : JSON.stringify(id),
+            dataType : 'json',
+            async : false,
+            success : function() {
+                window.location.href = 'http://localhost:8080/discover';
+            },
+        });
+    } );
+
+    let tableMyWorkout = $('#table-myworkout').DataTable( {
+        "paging": true,
+        "pagingType": "simple",
+        "searching": true,
+        "language": {
+            "decimal":        ",",
+            "emptyTable":     "Lista vazia",
+            "info":           "",
+            "infoEmpty":      "",
+            "infoFiltered":   "(filtrado de _MAX_ exercícios ao todo)",
+            "infoPostFix":    "",
+            "thousands":      " ",
+            "lengthMenu":     "Mostrar _MENU_ exercícios",
+            "loadingRecords": "Carregando...",
+            "processing":     "Processando...",
+            "search":         "Buscar:",
+            "zeroRecords":    "Nenhum exercício correspondente encontrado",
+            "paginate": {
+                "first":      "Ínicio",
+                "last":       "Fim",
+                "next":       "Próximo",
+                "previous":   "Anterior"
+            },
+            "aria": {
+                "sortAscending":  ": ative para ordenar coluna em ordem crescente",
+                "sortDescending": ": ative para ordenar coluna em ordem decrescente"
+            }
+        },
+        columns: [
+            {
+                "data": "id",
+                visible: false,
+            },
+            { "data": "name" },
+            { "data": "category" },
+            { "data": "difficulty" },
+            { "data": "link" },
+            { orderable: false },
+        ]
     } );
 
     $('#table-myworkout tbody').on('click', '.btn', function () {
         let row = $(this).closest('tr');
-        let data = table.row(row).data().name;
-        console.log(data);
+        let id = tableMyWorkout.row(row).data().id;
+        $.ajax("http://localhost:8080/user/exercises", {
+            type : 'DELETE',
+            contentType : 'application/json',
+            data : JSON.stringify(id),
+            dataType : 'json',
+            async : false,
+            success : function() {
+                window.location.href = 'http://localhost:8080/myworkout';
+            },
+        });
     } );
 } );
