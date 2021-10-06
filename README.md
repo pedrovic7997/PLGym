e78a9eb985d35517276e3227e5e54611de7dc701
pedrovic7997 <pedrovictor6974@gmail.com>
2021-10-06 19:43:11 -0300

    Beginning of javadoc notations and README
    Co-authored-by: leodeorce <leodeorce@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # PLGym
-Aplicação Web desenvolvida para a disciplina Projeto Integrado I.
+Aplicação Web desenvolvida para a disciplina Projeto Integrado I, lecionada pelo professor João Paulo Andrade Almeida.
+
+
+
+# Sobre
+O PLGym é uma ferramenta que facilita e organiza os seus exercícios físicos em casa. Cada usuário é capaz de adicionar em sua lista pessoal os exercícios que desejar dentre os fornecidos no nosso banco de dados. Todos os exercícios possuem um endereço de acesso a um vídeo tutorial (curado pela nossa equipe) sobre como melhor executá-lo.
+
+### Ferramentas usada
+
+* Spring Boot
+* Spring Security
+* Bootstrap
+* JQuery
+* Thymeleaf
+* Maven
+
+# Como instalar
+
+Para utilizar a ferramenta, basta seguir as seguintes instruções. As instruções são voltadas para uma máquina usando Linux.
+
+### Pré-requisitos
+
+* mvn (Maven)
+```sh
+sudo apt install maven
+```
+### Instalação
+
+* Clone o repositório
+```sh
+git clone https://github.com/pedrovic7997/PLGym.git
+```
+
+* Abra o diretório criado e acesse a pasta "back-app"
+
+* Dentro de "back-app", execute o comando
+```sh
+mvn spring-boot:run
+```
+Neste momento, depois de instalar diversos arquivos, o Spring entrará em atividade, sinalizando que a aplicação estará ativa.
+
+Para acessar, entre no seu navegador de internet e digite
+```
+localhost:8080
+```
+
+# Exemplo de uso
+
+Para ilustrar alguns exemplos de uso, temos abaixo um video demonstrando a navegação no site.
+
+<iframe width="864" height="486" src="https://www.youtube.com/embed/9r8Xv2tUghc?start=29&end=182;" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+**OBS:** Na versão atua do programa, o cadastro está funcionando porém para acessar com a conta criada, por motivos técnicos, o comando que liga o Spring deverá ser refeito para reiniciar a aplicação e assim fazer o login utilizando a conta nova.
+
+
+* 4 - Como organizar o setup de desenvolvimento
+* 5 - Como enviar uma modificação
+* 6 - Log de modificações
+* 7 - Licença e informações do autor
--- back-app/src/main/java/plgym/BackAppHtmlController.java
+++ back-app/src/main/java/plgym/BackAppHtmlController.java
@@ -13,56 +13,79 @@ import plgym.domain.ExerciseList;
 import plgym.domain.User;
 
 @Controller
-public class BackAppHtmlController
-{
+public class BackAppHtmlController {
+	/**
+	 * Returns index page as HTTP response.
+	 * 
+	 * @return Name of HTML file located in back-app/src/main/resources/templates
+	 */
 	@GetMapping("/")
-	public String index()
-	{
+	public String index() {
 		return "index";
 	}
 
+	/**
+	 * Returns login page as HTTP response.
+	 * 
+	 * @return Name of HTML file located in back-app/src/main/resources/templates
+	 */
 	@RequestMapping("/login")
-    public String login()
-	{
-        return "login";
-    }
+	public String login() {
+		return "login";
+	}
 
+	/**
+	 * Filters exercises the user has added from the exercise database and uses it to
+	 * return user-specific page as HTTP response.
+	 * 
+	 * @param model Object necessary for adding attributes to be handled by Thymeleaf
+	 *              on the HTML page
+	 * @param principal Object housing relevant logged in user's info from the session
+	 * @return Name of HTML file located in back-app/src/main/resources/templates
+	 */
 	@RequestMapping("/myworkout")
-    public String myWorkout(Model model, Principal principal)
-	{
+	public String myWorkout(Model model, Principal principal) {
 		User user = BackAppController.userDB.getValue(principal.getName());
 		String[] userExercisesId = user.getExercisesId().toArray(new String[0]);
-		ExerciseList userExercises = new ExerciseList(
-				userExercisesId,
-				BackAppController.exerciseDB,
-				new TypeToken<Map<String, Exercise>>(){}
-		);
+		ExerciseList userExercises = new ExerciseList(userExercisesId, BackAppController.exerciseDB,
+				new TypeToken<Map<String, Exercise>>() { });
 		model.addAttribute("exercises", userExercises);
-        return "myworkout";
-    }
+		return "myworkout";
+	}
 
+	/**
+	 * Returns signup page as HTTP response.
+	 * 
+	 * @return Name of HTML file located in back-app/src/main/resources/templates
+	 */
 	@RequestMapping("/signup")
-	public String signup()
-	{
+	public String signup() {
 		return "signup";
 	}
 
+	/**
+	 * Returns discover page as HTTP response.
+	 * 
+	 * @param model Object necessary for adding attributes to be handled by Thymeleaf
+	 * 				on the HTML page
+	 * @return Name of HTML file located in back-app/src/main/resources/templates
+	 */
 	@RequestMapping("/discover")
-	public String discover(Model model)
-	{
+	public String discover(Model model) {
 		model.addAttribute("exerciseDB", BackAppController.exerciseDB);
 		return "discover";
 	}
 
-	@RequestMapping("/suggestions")
-	public String suggestions()
-	{
-		return "suggestions";
-	}
-
+	/**
+	 * Returns profile page as HTTP response.
+	 * 
+	 * @param model Object necessary for adding attributes to be handled by Thymeleaf
+	 * 				on the HTML page
+	 * @param principal Object housing relevant logged in user's info from the session
+	 * @return Name of HTML file located in back-app/src/main/resources/templates
+	 */
 	@RequestMapping("/profile")
-	public String profile(Model model, Principal principal)
-	{
+	public String profile(Model model, Principal principal) {
 		model.addAttribute("user", BackAppController.userDB.getValue(principal.getName()));
 		return "profile";
 	}
